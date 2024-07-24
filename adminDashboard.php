437657eba1c29c5b7b533d84eeadf3e7e802da36
SaadiDK-003 <pcremote059@gmail.com>
2024-07-25 04:45:17 +0500

    update 2.1.2 change

--- adminDashboard.php
+++ adminDashboard.php
@@ -135,7 +135,7 @@ if (isLoggedin() === false || $userRole != 'admin') {
         <div class="toast align-items-center text-white bg-danger border-0" data-bs-animation="true" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="d-flex">
                 <div class="toast-body">
-                    Hello, world! This is a toast message.
+                    User has been deleted.
                 </div>
                 <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
